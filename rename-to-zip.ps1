bc81fab367f676102744e7ae590de4a02bf4c113
Mohammad Rafiq <rafiq@rrv.sh>
2025-05-19 14:11:52 +0800

    fix(rename-to-zip.ps1): fix function names

--- rename-to-zip.ps1
+++ rename-to-zip.ps1
@@ -1,4 +1,4 @@
-$files = Get-Children -Path . -File
-for ($file in $files) {
+$files = Get-ChildItem -Path . -File
+foreach ($file in $files) {
   Rename-Item -Path $file.FullName -NewName $($file.FullName + ".zip") -ErrorAction Stop
 }
