43b8ee51e5217f91eed4be2f86c4d5c407255d65
Mohammad Rafiq <rafiq@rrv.sh>
2025-05-23 13:45:31 +0800

    feat(check-latest-comment.py): add exclude list of TSEs

--- jira/check-latest-comment.py
+++ jira/check-latest-comment.py
@@ -15,15 +15,23 @@ def main():
     jira_pat = os.environ["CVT_JIRA_KEY"]
     jira = JIRA(server=jira_link, token_auth=jira_pat)
     # Define list of TSE
+    TSEList = [
+        "mohammad.rafiq",
+        "karishmma",
+        "vinotheni",
+        "piramilah",
+        "taqiuddin",
+        "jimi.mirza",
+        "Xavier Selvanathan (XTR)",
+    ]
     # Get all issues that are not closed
     for issue in jira.search_issues(
         'project != CWPEVT and status not in (Closed, "Pending Clarification", Resolved)  ORDER BY created DESC',
         maxResults=100,
     ):
         latest_comment = get_latest_comment_object(issue)
-        if not latest_comment:
-            continue
-        print(f"{issue.key}: {latest_comment.author}")
+        if latest_comment and latest_comment.author.displayName not in TSEList:
+            print(f"{issue.key}: {latest_comment.author.displayName}")
     # def empty list
     # For each:
     # Check who made the last comment
