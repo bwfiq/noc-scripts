cbc08ed5baf0b7021ab632ba2db6c5dd7a152d61
Mohammad Rafiq <rafiq@rrv.sh>
2025-05-23 12:27:31 +0800

    chore(check-latest-comment.py): add psuedo code

--- jira/check-latest-comment.py
+++ jira/check-latest-comment.py
@@ -2,11 +2,19 @@ import os
 import sys
 from jira import JIRA
 
+
 def main():
     jira_link = os.environ["CVT_JIRA_LINK"]
-    print(jira_link)
     jira_pat = os.environ["CVT_JIRA_KEY"]
     jira = JIRA(server=jira_link, token_auth=jira_pat)
+    # Define list of TSE
+    # Get all issues that are not closed
+    # def empty list
+    # For each:
+    # Check who made the last comment
+    # If not TSE, add to list
+    # print list
+
 
 if __name__ == "__main__":
     main()
